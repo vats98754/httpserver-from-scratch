b8312d0724228353985a5e2a3db7a38c346b99ed
vats98754 <a7vats@uwaterloo.ca>
2025-07-26 01:37:13 +0530

    add routes and route handler within wsgiapp, so routes are handled on the framework's end

--- wsgiapp.py
+++ wsgiapp.py
@@ -1,3 +1,12 @@
+routes = {}
+
+def route(path, methods=['GET']):
+    def decorator(func):
+        for method in methods:
+            routes[(method, path)] = func
+        return func
+    return decorator
+
 def app(env, start_response):
     """Barebones WSGI application wherein we create our own Web framework"""
     status = '200 OK'
@@ -7,4 +16,33 @@ def app(env, start_response):
         ('Content-Length', str(len(response_body)))
     ]
     start_response(status, response_headers)
+    return [response_body]
+
+@route('/')
+def home(env, start_response):
+    status = '200 OK'
+    response_body = b'Welcome to home page with route /'
+    response_headers = [
+        ('Content-Type', 'text/plain'),
+        ('Content-Length', str(len(response_body)))
+    ]
+    start_response(status, response_headers)
+    return [response_body]
+
+@route('/api/users', methods=['GET', 'POST'])
+def users_api(env, start_response):
+    method = env['REQUEST_METHOD']
+    status = '200 OK'
+
+    if method == 'GET':
+        response_body = b'{"users": ["anvay", "vats"]}'
+        content_type = 'application/json'
+    else: # POST request
+        response_body = b'{"message": "User created sucessfully!"]}'
+        content_type = 'application/json'
+    response_headers = [
+        ('Content-Type', content_type),
+        ('Content-Length', str(len(response_body)))
+    ]
+    start_response(status, response_body)
     return [response_body]
